801927bd7e609ef4a0bd2fe617ea5ee3dd125b78
mackilroy007 <mackilroy007@gmail.com>
2020-11-11 14:21:58 +0100

    bootstraped design version

--- crud/update.php
+++ crud/update.php
@@ -17,6 +17,13 @@ if ($_GET['id']) {
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
    <title >Edit Meal</title>
 
    <style type= "text/css">
@@ -34,41 +41,82 @@ if ($_GET['id']) {
 </head>
 <body>
 
-<fieldset>
+<div class="container mt-5">
+    <h2 class="text-center m-4">Add a Meal</h2>
+        
+        <form class="" action="actions/a_update.php" method="post">
+            <div class="form-group my-4">
+                <label for="meal">Meal Name</label>
+                <input class="form-control" type="text" name="meal"  placeholder="Meal name" value="<?php echo $data['m_name'] ?>"/>
+            </div>
+            <div class="form-group my-4">
+                <label for="meal">Price</label>
+                <input class="form-control" type="number" name= "price" placeholder="Insert price" value="<?php echo $data['price'] ?>"/>
+            </div>
+            <div class="form-group my-4">
+                <label for="meal">Image URL Format</label>
+                <input class="form-control" type="text"  name="img" placeholder ="Insert img url" value="<?php echo $data['img'] ?>"/>
+            </div>
+            <div class="form-group my-4">
+                <label for="meal">Ingredients</label>
+                <input class="form-control" type="text"  name="ingredients" placeholder ="Insert ingredients" value="<?php echo $data['ingredients'] ?>"/>
+            </div>
+            <div class="form-group my-4">
+                <label for="meal">Allergies</label>
+                <input class="form-control" type="text"  name="allergies" placeholder ="Insert allergies" value="<?php echo $data['allergies'] ?>"/>
+            </div>
+            
+            <input type= "hidden" name= "id" value= "<?php echo $data['id']?>" />
+               
+            <!-- button to submit content -->
+            <button class="btn btn-success" type= "submit">Save Changes</button >
+            <!-- button to go back -->
+            <a href= "index.php"><button class="btn btn-dark" type="button">Back</button></a>
+
+        </form>
+</div>
+
+<!-- <fieldset>
    <legend>Update meal</legend>
 
    <form action="actions/a_update.php"  method="post">
        <table  cellspacing="0" cellpadding= "0">
            <tr>
                <th>Meal</th>
-               <td><input type="text"  name="meal" placeholder ="Add meal" value="<?php echo $data['meal'] ?>"  /></td>
+               <td><input type="text"  name="meal" placeholder ="Add meal" value="<?php #echo $data['meal'] ?>"  /></td>
            </tr >
            <tr>
                <th>price</th>
-               <td><input type="number"  name="price" placeholder ="Insert price" value="<?php echo $data['price'] ?>"  /></td>
+               <td><input type="number"  name="price" placeholder ="Insert price" value="<?php #echo $data['price'] ?>"  /></td>
            </tr > 
            <tr>
                <th>Image</th>
-               <td><input type="text"  name="img" placeholder ="Add image URL" value="<?php echo $data['img'] ?>"  /></td>
+               <td><input type="text"  name="img" placeholder ="Add image URL" value="<?php #echo $data['img'] ?>"  /></td>
            </tr >     
            <tr>
                <th>Ingredients</th>
-               <td><input type= "text" name="ingredients"  placeholder="Add ingredients" value ="<?php echo $data['ingredients'] ?>" /></td >
+               <td><input type= "text" name="ingredients"  placeholder="Add ingredients" value ="<?php #echo $data['ingredients'] ?>" /></td >
            </tr>
            <tr>
                <th >Allergies</th>
-               <td><input type ="text" name= "allergies" placeholder= "Add allergies" value= "<?php echo $data['allergies'] ?>"/></td>
+               <td><input type ="text" name= "allergies" placeholder= "Add allergies" value= "<?php #echo $data['allergies'] ?>"/></td>
            </tr>
            <tr>
-               <input type= "hidden" name= "id" value= "<?php echo $data['id']?>" />
+               <input type= "hidden" name= "id" value= "<?php #echo $data['id']?>" />
                <td><button  type= "submit">Save Changes</button ></td>
                <td><a  href= "index.php"><button  type="button">Back</button ></a ></td >
            </tr>
        </table>
    </form >
 
-</fieldset >
+</fieldset > -->
 
+<!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body >
 </html >
 
