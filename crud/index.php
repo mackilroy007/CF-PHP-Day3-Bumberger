8dd678a46161e3a233770b3079c3284a2f012890
mackilroy007 <mackilroy007@gmail.com>
2020-11-11 11:48:54 +0100

    added retrieving function

--- crud/index.php
+++ crud/index.php
@@ -34,7 +34,26 @@
            </tr>
        </thead>
        <tbody>
-           
+       <?php
+           $sql = "SELECT * FROM meal WHERE active = 0";
+           $result = $connect->query($sql);
+
+            if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                   echo  "<tr>
+                       <td>" .$row['m_name']." ".$row['price' ]."</td>
+                       <td>" .$row['price']."</td>.<td>" .$row['img']
+                       ."</td>.<td>" .$row['allergies']."</td>.<td>" .$row['ingridients']."</td>
+                       <td>
+                           <a href='update.php?id=" .$row['id']."'><button type='button'>Edit</button></a>
+                           <a href='delete.php?id=" .$row['id']."'><button type='button'>Delete</button></a>
+                       </td>
+                   </tr>" ;
+               }
+           } else  {
+               echo  "<tr><td colspan='5'><center>No Data Avaliable</center></td></tr>";
+           }
+            ?>
        </tbody>
    </table>
 </div>
